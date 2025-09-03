9a884971fa636b5b4d8970b606aecd64dd817e4f
mfabianchm <mfchongm@hotmail.com>
2025-09-03 15:46:44 -0600

    creation of context

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,12 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { AppContextProvider } from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </BrowserRouter>
 );
