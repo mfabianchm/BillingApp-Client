06aea655bf32f60f38ae7b90903fd0088e407fcd
mfabianchm <mfchongm@hotmail.com>
2025-09-03 00:29:46 -0600

    navbar and styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
+import MenuBar from "./components/MenuBar/MenuBar";
 
 function App() {
   return (
     <div>
+      <MenuBar></MenuBar>
       <Routes>
         <Route path="/" element={<h1>Hola Mundo</h1>} />
       </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
 
+//Import Bootstrap Libraries
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
